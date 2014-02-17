3e26323b1eb140f0f1aa540c513252c12eb13aa6
shidel-dev <joeshidel@gmail.com>
2014-02-17 14:36:08 -0600

    added the 3 files to commit. migrate, words class, and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,8 @@
+
+
 file = File.open('/usr/share/dict/words', "r")
-file.each_line
+file.each_line do |line|
+  line.strip!.downcase
+  Word.create({word: line, sorted_word: line.split("").sort!.join("")})
+end
+file.close
