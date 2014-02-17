0730cdd0265c36f04395b751b8227b4e4f21460d
shidel-dev <joeshidel@gmail.com>
2014-02-17 15:13:31 -0600

    working on web interface

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,4 +1,13 @@
 class Word < ActiveRecord::Base
   # Remember to create a migration!
 
+  def get_anagrams
+    anagrams = []
+    puts self.word
+    # Word.all.each do |word|
+    #   if word.sorted_word == self.sorted_word
+    #     anagrams << word
+    #   end
+    # end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@
 
 file = File.open('/usr/share/dict/words', "r")
 file.each_line do |line|
-  line.strip!.downcase
+  line.strip!.downcase!
   Word.create({word: line, sorted_word: line.split("").sort!.join("")})
 end
 file.close
