def65657496959937699874219b36bdaf1eaee1a
chrisscherer <chrisscherer90@gmail.com>
2014-02-17 17:55:16 -0600

    fixed launch error

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,7 @@
+get '/' do
+  erb :index
+end
+
 post '/' do
   @input_word = Word.new(word: params[:user_input], sorted_word: params[:user_input].split('').sort!.join(''))
   @anagrams = @input_word.get_anagrams
